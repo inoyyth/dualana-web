0f193e654c3ac36212ffae24d0b1b4741637149b
Inoyyth <supri170845@gmail.com>
2026-07-25 11:56:53 +0700

    fixed banner description disappear

--- resources/views/elements/hero.blade.php
+++ resources/views/elements/hero.blade.php
@@ -2,13 +2,13 @@
     <div class="hero-copy reveal">
       <img class="hero-logo" src="{{asset('assets/images/aset-logo.png')}}" alt="Dualana">
       @if($banner && isset($banner['data'][0]))
-        {!! $banner['data'][0]['content'] ?? '' !!}
+        {!! $banner['data'][0]['content'] !!}
         <div class="hero-actions">
-          @if(isset($banner['data'][0]['acf']['banner_cta_1_link']))
-            <a class="button primary" href="{{$banner['data'][0]['acf']['banner_cta_1_link']}}">  {{$banner['data'][0]['acf']['banner_cta_1_label'] ?? 'CTA 1'}} <span><i class="fa-regular fa-circle-check"></i></span></a>
+          @if(isset($banner['data'][0]['acf']['banner_cta_1_link']) && isset($banner['data'][0]['acf']['banner_cta_1_label']))
+            <a class="button primary" href="{{$banner['data'][0]['acf']['banner_cta_1_link']}}">  {{$banner['data'][0]['acf']['banner_cta_1_label']}} <span><i class="fa-regular fa-circle-check"></i></span></a>
           @endif
-          @if(isset($banner['data'][0]['acf']['banner_cta_2_link']))
-            <a class="button secondary" href="{{$banner['data'][0]['acf']['banner_cta_2_link']}}">{{$banner['data'][0]['acf']['banner_cta_2_label'] ?? 'CTA 2'}} <span><i class="fa-regular fa-circle-question"></i></span></a>
+          @if(isset($banner['data'][0]['acf']['banner_cta_2_link']) && isset($banner['data'][0]['acf']['banner_cta_2_label']))
+            <a class="button secondary" href="{{$banner['data'][0]['acf']['banner_cta_2_link']}}">{{$banner['data'][0]['acf']['banner_cta_2_label']}} <span><i class="fa-regular fa-circle-question"></i></span></a>
           @endif
         </div>
       @endif
